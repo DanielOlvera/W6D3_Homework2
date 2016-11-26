39027725b1594768757b2a6e0fb6f9e430b82dbc
DanielOlvera <josue_olver@yahoo.com.mx>
2016-11-26 09:49:30 -0500

    Added ViewPager, inflated and tested working

--- app/src/main/java/com/example/daniel/w6d3_homework2/fragmentmenu/MenuFragmentActivity.java
+++ app/src/main/java/com/example/daniel/w6d3_homework2/fragmentmenu/MenuFragmentActivity.java
@@ -1,5 +1,6 @@
 package com.example.daniel.w6d3_homework2.fragmentmenu;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import com.example.daniel.w6d3_homework2.R;
 
 public class MenuFragmentActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_fragment);
+
+        viewPager = (ViewPager) findViewById(R.id.amf_viewPager);
     }
 }
